428505c389d572a404796e1e0988b9fa51f09499
JoleneKearse <pharaohnutz@gmail.com>
2024-07-07 14:28:05 -0400

    chore: style genre list skeleton

--- src/components/GenreListSkeleton.tsx
+++ src/components/GenreListSkeleton.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "@chakra-ui/react";
 
 export function GenreListSkeleton() {
-  return <Skeleton height={6} marginY={4}></Skeleton>;
+  return <Skeleton height={6} marginY={4} borderRadius={8}></Skeleton>;
 }
