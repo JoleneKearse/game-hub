3f70fb5d7e5126c29002e2ef897c370df0397eeb
JoleneKearse <pharaohnutz@gmail.com>
2024-07-07 15:18:36 -0400

    feat: add ability to filter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,9 +5,14 @@ import useGames from "../hooks/useGames";
 import { GameCard } from "./GameCard";
 import { GameCardSkelton } from "./GameCardSkeleton";
 import { GameCardContainer } from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
 
-export function GameGrid() {
-  const { data, error, isLoading } = useGames();
+interface GameGridProps {
+  selectedGenre: Genre | null;
+}
+
+export function GameGrid({ selectedGenre }: GameGridProps) {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
@@ -20,12 +25,12 @@ export function GameGrid() {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
+            <GameCardContainer key={skeleton}>
               <GameCardSkelton key={skeleton} />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
+          <GameCardContainer key={game.id}>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
         ))}
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,16 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Image, List, ListItem } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/imgUrl";
 import { GenreListSkeleton } from "./GenreListSkeleton";
 
-export function GenreList() {
+interface GenreListProps {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+export function GenreList({ onSelectGenre }: GenreListProps) {
   const { data, isLoading, error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  // if (isLoading) return <Spinner />;
 
   if (error) return null;
   return (
@@ -23,7 +26,7 @@ export function GenreList() {
                 borderRadius={8}
                 src={getCroppedImageUrl(genre.image_background)}
               />
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">{genre.name}</Button>
             </HStack>
           </ListItem>
         ))}
