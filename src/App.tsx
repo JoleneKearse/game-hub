bb428608c8a7d062610ea2efc39f4d333bb1ca27
JoleneKearse <pharaohnutz@gmail.com>
2024-07-09 10:03:42 -0400

    feat: add sort functionality

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { SortSelector } from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -50,7 +51,11 @@ function App() {
             }
             selectedPlatform={gameQuery.platform}
           />
-          <SortSelector />
+          <SortSelector sortOrder={gameQuery.sortOrder}
+            onSelectSortOrder={(sortOrder) =>
+              setGameQuery({ ...gameQuery, sortOrder })
+            }
+          />
         </HStack>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
